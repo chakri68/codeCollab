0569b46cce900d24f7b78c36eec8a56f9af0f855
flyingdelta <flyingdelta@outlook.com>
2022-06-17 19:41:16 +0530

    primary color settings updated

--- public/scripts/componentScripts/codeScript.js
+++ public/scripts/componentScripts/codeScript.js
@@ -130,6 +130,8 @@ const cpyBehaviour = document.getElementById("copyBehaviour");
 const fontSize = document.getElementById("code-font-size");
 const keyMap = document.getElementById("keyMap");
 const tabSize = document.getElementById("tabSize");
+const color = document.getElementById("prmyColorBtn");
+const customToggle = document.getElementById("customToggle");
 let optionObj = {
   autoCloseBrackets: true,
   lineNumbers: true,
@@ -170,9 +172,140 @@ function codeEditorinit(obj) {
 //   e.currentTarget.style.opacity = "1";
 // }
 
+const rgb2hex = (rgb) =>
+  `#${rgb
+    .match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/)
+    .slice(1)
+    .map((n) => parseInt(n, 10).toString(16).padStart(2, "0"))
+    .join("")}`;
+
+function hexToHSL(H) {
+  // Convert hex to RGB first
+  let r = 0,
+    g = 0,
+    b = 0;
+  if (H.length == 4) {
+    r = "0x" + H[1] + H[1];
+    g = "0x" + H[2] + H[2];
+    b = "0x" + H[3] + H[3];
+  } else if (H.length == 7) {
+    r = "0x" + H[1] + H[2];
+    g = "0x" + H[3] + H[4];
+    b = "0x" + H[5] + H[6];
+  }
+  // Then to HSL
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  let cmin = Math.min(r, g, b),
+    cmax = Math.max(r, g, b),
+    delta = cmax - cmin,
+    h = 0,
+    s = 0,
+    l = 0;
+
+  if (delta == 0) h = 0;
+  else if (cmax == r) h = ((g - b) / delta) % 6;
+  else if (cmax == g) h = (b - r) / delta + 2;
+  else h = (r - g) / delta + 4;
+
+  h = Math.round(h * 60);
+
+  if (h < 0) h += 360;
+
+  l = (cmax + cmin) / 2;
+  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+
+  return "hsl(" + h + "," + s + "%," + l + "%)";
+}
+
+function HSLToHex(hsl) {
+  let sep = hsl.indexOf(",") > -1 ? "," : " ";
+  hsl = hsl.substr(4).split(")")[0].split(sep);
+
+  let h = hsl[0],
+    s = hsl[1].substr(0, hsl[1].length - 1) / 100,
+    l = hsl[2].substr(0, hsl[2].length - 1) / 100;
+
+  // Strip label and convert to degrees (if necessary)
+  if (h.indexOf("deg") > -1) h = h.substr(0, h.length - 3);
+  else if (h.indexOf("rad") > -1)
+    h = Math.round(h.substr(0, h.length - 3) * (180 / Math.PI));
+  else if (h.indexOf("turn") > -1)
+    h = Math.round(h.substr(0, h.length - 4) * 360);
+  if (h >= 360) h %= 360;
+  let c = (1 - Math.abs(2 * l - 1)) * s,
+    x = c * (1 - Math.abs(((h / 60) % 2) - 1)),
+    m = l - c / 2,
+    r = 0,
+    g = 0,
+    b = 0;
+
+  if (0 <= h && h < 60) {
+    r = c;
+    g = x;
+    b = 0;
+  } else if (60 <= h && h < 120) {
+    r = x;
+    g = c;
+    b = 0;
+  } else if (120 <= h && h < 180) {
+    r = 0;
+    g = c;
+    b = x;
+  } else if (180 <= h && h < 240) {
+    r = 0;
+    g = x;
+    b = c;
+  } else if (240 <= h && h < 300) {
+    r = x;
+    g = 0;
+    b = c;
+  } else if (300 <= h && h < 360) {
+    r = c;
+    g = 0;
+    b = x;
+  }
+  // Having obtained RGB, convert channels to hex
+  r = Math.round((r + m) * 255).toString(16);
+  g = Math.round((g + m) * 255).toString(16);
+  b = Math.round((b + m) * 255).toString(16);
+
+  // Prepend 0s, if necessary
+  if (r.length == 1) r = "0" + r;
+  if (g.length == 1) g = "0" + g;
+  if (b.length == 1) b = "0" + b;
+
+  return "#" + r + g + b;
+}
+
 function handleTabSize(e) {
   myCodeMirror[0].setOption("indentUnit", parseInt(e.target.value));
   myCodeMirror[0].setOption("tabSize", parseInt(e.target.value));
+}
+
+function handleCustomToggle(e) {
+  if (e.target.checked) {
+    // color.disabled = false;
+    root.style.setProperty("--link-primary", color.value);
+  } else {
+    // color.disabled = true;
+    root.style.setProperty(
+      "--link-primary",
+      "hsl(var(--text-h), var(--text-s), var(--text-l))"
+    );
+    // try {
+    //   color.value = rgb2hex(
+    //     window
+    //       .getComputedStyle(document.querySelector(".btn"))
+    //       .getPropertyValue("background-color")
+    //   );
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  }
 }
 
 function setTheme(obj, theme) {
@@ -200,9 +333,20 @@ function setTheme(obj, theme) {
       root.style.setProperty("--text-primary", "white");
       root.style.setProperty("--text-h", "var(--bg-rgbh1)");
       root.style.setProperty("--text-s", "calc(var(--bg-rgbs1) * 4)");
-      root.style.setProperty("--text-l", "calc(var(--bg-rgbl1) * 4)");
+      root.style.setProperty("--text-l", "calc(var(--bg-rgbl1) * 3.75)");
     }
   }
+  // if (!customToggle.ariaChecked) {
+  //   try {
+  //     color.value = rgb2hex(
+  //       window
+  //         .getComputedStyle(document.querySelector(".btn"))
+  //         .getPropertyValue("background-color")
+  //     );
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 }
 
 function currentTheme(e) {
@@ -500,9 +644,11 @@ function init() {
     }
   });
   // JS for the primary color toggle
-  const color = document.getElementById("prmyColorBtn");
+
+  customToggle.addEventListener("change", handleCustomToggle);
+
   color.addEventListener("change", () => {
-    if (color.value != rs.getPropertyValue("--link-primary")) {
+    if (customToggle.ariaChecked) {
       if (
         confirm(`Are you sure you want to change the color to ${color.value}?`)
       ) {

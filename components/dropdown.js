bf3f973e0f05a64d32601df9834975ea873f825d
flyingdelta <flyingdelta@outlook.com>
2022-06-18 12:13:44 +0530

    updated dropdown

--- components/dropdown.js
+++ components/dropdown.js
@@ -5,20 +5,36 @@ export default function Dropdown({ title, bodyJSX, className }) {
         className={`dropdown-trigger dotted-border ${
           className ? className : ""
         }`}
+        style={{
+          display: "grid",
+          gridAutoFlow: "column",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
       >
-        <p aria-haspopup="true" aria-controls="dropdown-menu">
-          <span>{title}</span>
-          <span className="icon is-small">
-            <i className="fas fa-angle-down" aria-hidden="true"></i>
-          </span>
-        </p>
+        <span>{title}</span>
+        <label
+          className="svgButton hvrBtn"
+          style={{
+            height: "2rem",
+          }}
+        >
+          <svg
+            style={{ position: "relative", bottom: "7px" }}
+            xmlns="http://www.w3.org/2000/svg"
+            height="48"
+            width="48"
+          >
+            <path d="M24 30 14 20.05H34Z" />
+          </svg>
+        </label>
       </div>
       <div
         className="dropdown-menu height-transitions"
         role="menu"
         style={{
           backgroundColor: "transparent",
-          // display: "grid",
+          display: "grid",
           gridTemplateColumns: "auto 100px",
           justifyContent: "space-between",
           justifyItems: "stretch",

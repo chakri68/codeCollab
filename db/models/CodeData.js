6d3d79c3eaf5b9a03a0b89332e1d32c26111d29a
flyingdelta <flyingdelta@outlook.com>
2022-07-16 18:29:39 +0530

    database code

---
+++ db/models/CodeData.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+/* CodeData will correspond to a collection in your MongoDB database. */
+const CodeData = new mongoose.Schema({
+  title: {
+    // Title of the code
+
+    type: String,
+    required: [true, "Please provide a name for this code."],
+    maxlength: [60, "Name cannot be more than 60 characters"],
+  },
+  author: {
+    // The author of the code
+
+    type: String,
+    maxlength: [60, "Author's name cannot be more than 60 characters"],
+  },
+  language: {
+    // The language the code was written in
+
+    type: String,
+    required: [true, "Please specify the language."],
+    maxlength: [40, "language can't be more than 40 characters"],
+  },
+  code: {
+    //   The code
+    type: String,
+    required: [true, "The code cannot be empty"],
+    maxlength: [4000, "Code can't be more than 2000 characters long"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  expiresAt: {
+    type: Date,
+  },
+});
+
+CodeData.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+
+export default mongoose.models.CodeData || mongoose.model("CodeData", CodeData);

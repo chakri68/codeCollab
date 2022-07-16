593a50d9286f5d8669745d82f720f0737255e039
flyingdelta <flyingdelta@outlook.com>
2022-07-16 18:39:25 +0530

    landing page for cloudcode

---
+++ pages/[pasteId].js
@@ -0,0 +1,81 @@
+import Header from "../components/header";
+import Menu from "../components/menu";
+import Head from "next/head";
+import ModalPopUp from "../components/modal";
+import Footer from "../components/footer";
+import Script from "next/dist/client/script";
+import ToolBar from "../components/toolbar";
+import Codeview from "../components/codeview";
+import dbConnect from "../db/lib/dbConnect";
+import Code from "../db/models/CodeData.js";
+import Minmenu from "../components/minMenu";
+// import { nanoid } from "nanoid";
+
+export default function CloudCode({
+  code: { title, author, language, code, expiresAt, createdAt },
+  link,
+}) {
+  return (
+    <>
+      <Head>
+        <title>codeCollab | {title}</title>
+      </Head>
+      <Header />
+      <ToolBar title={title} link={link} />
+      <div id="fullScreen">
+        <Minmenu
+          items={[
+            { id: 1, label: "home", url: "/" },
+            { id: 2, label: "login", url: "/" },
+            {
+              id: 3,
+              label: "github",
+              url: "https://github.com/chakri68/codeCollab",
+            },
+            { id: 4, label: "api", url: "/" },
+            {
+              id: 5,
+              label: "changelog",
+              url: "https://github.com/chakri68/codeCollab/tree/main",
+            },
+            { id: 6, label: "legal", url: "/" },
+          ]}
+        />
+        <ModalPopUp
+          id="customModal"
+          titleJSX="Custom"
+          className="pop-down"
+          options={
+            <>
+              <p>Custom</p>
+            </>
+          }
+        />
+        <Codeview langauge={language} code={code} />
+      </div>
+      <Footer />
+      <Script src="scripts/componentScripts/modalScript.js"></Script>
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  await dbConnect();
+  try {
+    let doc = await Code.findById(params.pasteId, { _id: 0 }).lean();
+    doc.createdAt = doc.createdAt.toString();
+    doc.expiresAt = doc.createdAt.toString();
+    console.log(doc);
+    return {
+      props: {
+        code: doc,
+        link: params.pasteId,
+      },
+    };
+  } catch (error) {
+    return {
+      notFound: true,
+    };
+  }
+}

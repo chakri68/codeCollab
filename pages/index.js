3e4396b60a49391cb3aa0b7bf01702a4ee5456d8
flyingdelta <flyingdelta@outlook.com>
2022-07-21 18:37:42 +0530

    added togetherJS

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,105 @@ import ModalPopUp from "../components/modal";
 import Footer from "../components/footer";
 import Script from "next/dist/client/script";
 import MenuBox from "../components/MenuBox";
+import { useEffect } from "react";
 
 export default function Home() {
+  function waitForElm(selector) {
+    return new Promise((resolve) => {
+      if (document.querySelector(selector)) {
+        return resolve(document.querySelector(selector));
+      }
+
+      const observer = new MutationObserver((mutations) => {
+        if (document.querySelector(selector)) {
+          resolve(document.querySelector(selector));
+          observer.disconnect();
+        }
+      });
+
+      observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+      });
+    });
+  }
+  function togetherJSinit() {
+    window.BeforeUnloadEvent = () => {
+      TogetherJS(this);
+    };
+    window.TogetherJSConfig_hubBase = "https://codecollab-hub.herokuapp.com";
+    //JQuery drag issues fix, declare it before togetherjs
+    // Make the DIV element draggable:
+    waitForElm("#togetherjs-dock").then((elmnt) => dragElement(elmnt));
+
+    function dragElement(elmnt) {
+      console.log(elmnt);
+      var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
+      if (document.getElementById(elmnt.id + "header")) {
+        // if present, the header is where you move the DIV from:
+        document.getElementById(elmnt.id + "header").onmousedown =
+          dragMouseDown;
+      } else {
+        // otherwise, move the DIV from anywhere inside the DIV:
+        elmnt.onmousedown = dragMouseDown;
+      }
+
+      function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+      }
+
+      function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+        elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+      }
+
+      function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+      }
+    }
+  }
+  useEffect(() => {
+    togetherJSinit();
+  }, []);
   return (
     <>
       <Head>
         <title>codeCollab - Edit and Share Code!</title>
       </Head>
       <Header />
+      <button
+        title="Start a collaborative sesssion"
+        className="button btn is-size-6 has-text-weight-medium hvrBtn"
+        id="start-togetherjs"
+        type="button"
+        onClick={() => {
+          TogetherJS(this);
+          return false;
+        }}
+        data-end-togetherjs-html="End TogetherJS"
+      >
+        Start a session
+      </button>
       <div id="fullScreen">
         <Menu
           items={[
@@ -104,7 +195,8 @@ export default function Home() {
         />
       </div>
       <Footer />
-      <Script src="scripts/componentScripts/modalScript.js"></Script>
+      <Script src="scripts/componentScripts/modalScript.js" />
+      <Script src="https://togetherjs.com/togetherjs-min.js" />
     </>
   );
 }
